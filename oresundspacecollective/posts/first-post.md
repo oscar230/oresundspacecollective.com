ede3d50947764957e69af7d4731081cbd9f15474
Oscar Andersson <oscaande@outlook.com>
2024-07-24 14:28:47 +0200

    Revert "Add basic blog"
    This reverts commit 5892129809a1f8109fe01352fc5f217b16128028.

--- oresundspacecollective/posts/first-post.md
+++
@@ -1,8 +0,0 @@
----
-title: "First Post"
-date: "2024-07-24"
----
-
-# My First Blog Post
-
-Welcome to my first blog post written in Markdown!
--- oresundspacecollective/vite.config.ts
+++ oresundspacecollective/vite.config.ts
@@ -1,10 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import md from 'vite-plugin-md';
 
 export default defineConfig({
-	plugins: [
-	  sveltekit(),
-	  md()
-	]
+	plugins: [sveltekit()]
 });
