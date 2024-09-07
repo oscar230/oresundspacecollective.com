785e8808f1d0aea5a342a2bbf5afe1b858d7c883
Oscar Andersson <oscar.andersson.200@gmail.com>
2024-09-07 14:34:17 +0200

    Updates compare script

--- compare.sh
+++ compare.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # Directories to compare
-DIR1="/path/to/directory1"
-DIR2="/path/to/directory2"
+DIR1="src-todo"
+DIR2="osc"
 
 # Find all files (recursively) in both directories, extract filenames without the path
 files_in_dir1=$(find "$DIR1" -type f -exec basename {} \;)
 files_in_dir2=$(find "$DIR2" -type f -exec basename {} \;)
-
-echo "Files with the same name in both directories:"
 
 # Compare file names
 for file in $files_in_dir1; do
